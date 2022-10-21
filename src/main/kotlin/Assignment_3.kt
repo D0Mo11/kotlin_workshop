001d1b52f44423c69c025fa740ac11724b59cc35
Domagoj Dragić <62610228+D0Mo11@users.noreply.github.com>
2022-10-21 14:41:16 +0200

    changed Assignment_3

--- src/main/kotlin/Assignment_3.kt
+++ src/main/kotlin/Assignment_3.kt
@@ -1,17 +1,39 @@
 /**
- * For given string of words print out the longest word and its length.
+ * Two teams play a basketball match. Given the list of
+ * <player, team, points> for each player in
+ * the match, we have to get the match scoreboard.
  */
+enum class Team {
+    YELLOW, BLUE
+}
 
-private const val input = "Who solves this fastest and cleanest gets a beer from Domagoj"
+data class PlayerGame(
+    val player: String,
+    val team: Team,
+    val points: Int
+)
+
+val match = listOf(
+    PlayerGame("13", Team.YELLOW, 12),
+    PlayerGame("7", Team.YELLOW, 7),
+    PlayerGame("3", Team.YELLOW, 20),
+    PlayerGame("22", Team.YELLOW, 16),
+    PlayerGame("33", Team.YELLOW, 4),
+    PlayerGame("45", Team.YELLOW, 26),
+    PlayerGame("8", Team.BLUE, 17),
+    PlayerGame("3", Team.BLUE, 15),
+    PlayerGame("21", Team.BLUE, 16),
+    PlayerGame("30", Team.BLUE, 12),
+    PlayerGame("45", Team.BLUE, 24)
+)
 
 fun main() {
     //TODO Uncomment following line and write your solution to match the expected output
-    // println("Longest word is: ${output?.first} with length ${output?.second}")
+    // println("Yellow team score is $yellowTeamPoints and blue team score $blueTeamPoints.")
 }
 
 /**
- * Expected output:
+ * Output:
  *
- * Longest word is: cleanest with length 8
- *
+ * Yellow team score is 85 and blue team score 84.
  */
